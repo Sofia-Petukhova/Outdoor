c43aa4892c1469d9bb0b744f56e8c5942ed661cb
Sofia-Petukhova <sofiapetukhova7@gmail.com>
2022-11-20 19:25:25 +0300

    added adaptive to iPad Air

---
+++ script.js
@@ -0,0 +1,5 @@
+let headerBurger = document.querySelector('.header__burger');
+let headerMenu = document.querySelector('.header__burger_menu');
+headerBurger.addEventListener('click', () => {
+	headerMenu.classList.toggle('active')}
+)
